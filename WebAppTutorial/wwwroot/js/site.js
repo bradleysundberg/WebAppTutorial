3f51f21ad85dacf31d0a5e8d7583f0184bbe99e3
Brad <Brad@DESKTOP-60H04DP>
2025-05-12 13:15:08 -0700

    Add project files.

---
+++ WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const heading = document.getElementById('animated-heading');
+
+    function fadeEffect() {
+        let opacity = 1;
+        let fadingOut = true;
+
+        setInterval(() => {
+            if (fadingOut) {
+                opacity -= 0.02;
+                if (opacity <= 0.3) fadingOut = false;
+            } else {
+                opacity += 0.02;
+                if (opacity >= 1) fadingOut = true;
+            }
+            heading.style.opacity = opacity;
+        }, 50);
+    }
+
+    fadeEffect();
+});
